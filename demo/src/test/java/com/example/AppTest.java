5dba6027bbdfb1cf7b111e22a74808c7231661c4
gn-ourida <guenourida@gmail.com>
2024-05-04 10:12:13 +0100

    add java doc

--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -5,33 +5,39 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class AppTest
-{
+public class AppTest {
 
+    /**
+     * Test to verify that an IllegalArgumentException is thrown when the number is null.
+     */
     @Test(expected = IllegalArgumentException.class)
     public void test_fact_raises_exception_for_null_number() {
         Integer number = null;
-        Integer power = 1
-        ;
-        App.power(number,power);
+        Integer power = 1;
+        App.power(number, power);
     }
 
+    /**
+     * Test to verify that any number raised to the power of 0 returns 1.
+     */
     @Test
-    public void power_0_and_numper_0_returns_1()
-    {
+    public void power_0_and_number_0_returns_1() {
         assertEquals(1.0, App.power(0, 0), 0.0001);
     }
 
+    /**
+     * Test to verify that any number raised to the power of 0 returns 1.
+     */
     @Test
-    public void power_0_and_any_namper_returns_1()
-    {
+    public void power_0_and_any_number_returns_1() {
         assertEquals(1.0, App.power(1, 0), 0.0001);
     }
 
-    public void power_2_and_numper_2_returns_4()
-    {
-        assertEquals(4.0, App.power(2,2 ), 0.0001);
+    /**
+     * Test to verify that 2 raised to the power of 2 returns 4.
+     */
+    @Test
+    public void power_2_and_number_2_returns_4() {
+        assertEquals(4.0, App.power(2, 2), 0.0001);
     }
-
-    
 }
